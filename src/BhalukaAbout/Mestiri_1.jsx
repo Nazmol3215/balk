9ead685607a40fb0be870113e958b8bea8f9b0ae
Nazmol3215 <mdasikahamad2006@gmail.com>
2025-05-10 10:29:52 +0600

    first00 commit

--- src/BhalukaAbout/Mestiri_1.jsx
+++ src/BhalukaAbout/Mestiri_1.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import Footer from '../Layout/Footer';
 import OffcanvasExample from '../Layout/OffcanvasExample';
 // import Mestiri from "../Test/Mestiri";
-import UserList from '../Admin/UserList';
+// import UserList from '../Admin/UserList';
 
 
 
 export default function Mestiri_1() {
   return (
     <div>
+        <div className="container">
+          <h1 className="text-center">মেস্তিরী</h1>
+          <p className="text-center">বালুকা উপজেলার মেস্তিরী</p>
+             </div>
         <OffcanvasExample />
         {/* <Mestiri/> */}
-        <UserList/>
+        {/* <UserList/> */}
       <Footer />
     </div>
   )
